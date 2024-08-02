a9de069a25b6cac5bd71ca9dbc1e5ef2bb75d791
Forexium <150202229+Forexium@users.noreply.github.com>
2024-08-01 23:57:49 -0700

    Update Modules.lua

--- Modules.lua
+++ Modules.lua
@@ -1,7 +1,5 @@
 local Yon_Module = {}
 
-local Players = game:GetService("Players")
-
 function Yon_Module.GetBall()
     for i, v in workspace.Balls:GetChildren() do
         if v:GetAttribute("realBall") then
